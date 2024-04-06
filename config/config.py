2d0ce74862e77927af9fcf7593ee43fdf07e4f7f
Fernando Zurita <ferzuritag@hotmail.com>
2024-04-06 14:44:00 -0600

    add: config

---
+++ config/config.py
@@ -0,0 +1,18 @@
+import os
+
+config = {
+    'available_services': {
+        'vrp': {
+            'name': 'PSroblema de Ruteo de Vehiculos',
+            'path':  os.getenv('VRP_API_PATH')
+        },
+        'cvrp': {
+            'name': 'Problema de Ruteo de Vehiculos con Capacidad Limitada',
+            'path':  os.getenv('CVRP_API_PATH')
+        },
+        'mdvrp': {
+            'name': 'Problema de Ruteo de Vehiculos con Capacidad Limitada',
+            'path':  os.getenv('MDVRP_API_PATH')
+        }
+    }
+}
